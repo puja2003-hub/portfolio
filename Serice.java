11b580addd2bba68b0db1651149d6999ab8b93f6
puja2003-hub <154983577+puja2003-hub@users.noreply.github.com>
2023-12-27 17:42:06 +0530

    portfolio

---
+++ Serice.java
@@ -0,0 +1,12 @@
+class Serice
+{
+      public static void main(String args[])
+      {
+         int i=1;
+         for(int i=2;i<=10;i++)
+          {
+              j=i*i*i;    
+              System.out.print(j+"+");
+         }
+     }
+}
